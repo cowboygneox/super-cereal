29f1d8386ff932a53edd1bcfd992c566c9296432
Sean Freitag <sean.freitag@avant.com>
2023-01-19 12:29:42 -0600

    Adding more tests to the JsonCerealizer.

--- tests/super_cereal/cerealizer/test_json.py
+++ tests/super_cereal/cerealizer/test_json.py
@@ -1,9 +1,31 @@
+import dataclasses
+from typing import Optional
+
 import pytest
 
 from super_cereal.cerealizer.json import JsonCerealizer
 
 
 @pytest.mark.parametrize('obj', ['stuff', 42, 12.552, True])
-def test_serializer(obj: any):
+def test_serializer_primatives(obj: any):
     cerealizer = JsonCerealizer()
-    assert cerealizer.deserialize(cerealizer.serialize(obj), str) == obj
+    serialized = cerealizer.serialize(obj)
+    assert serialized == obj
+    assert cerealizer.deserialize(serialized, type(obj)) == obj
+
+
+def test_serializer_simple_object():
+    @dataclasses.dataclass
+    class TestClass:
+        field1: str
+        field2: int
+        field3: float
+        field4: bool
+        field5: Optional[str]
+        field6: Optional[str]
+
+    cerealizer = JsonCerealizer()
+    obj = TestClass('stuff', 42, 12.552, True, 'another', None)
+    serialized = cerealizer.serialize(obj)
+    deserialized = cerealizer.deserialize(serialized, TestClass)
+    assert obj == deserialized
