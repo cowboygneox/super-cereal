64adc499a0f31a9c58ec8c2f2c2c50bb335c1192
Sean Freitag <sean.freitag@avant.com>
2023-01-19 13:58:45 -0600

    Adding a more complex case for the JsonCerealizer.

--- super_cereal/cerealizer/json.py
+++ super_cereal/cerealizer/json.py
@@ -9,6 +9,8 @@ class JsonCerealizer(Cerealizer[T, typing.Union[str, float, int, bool, type(None
     def serialize(self, obj: T) -> typing.Union[str, float, int, bool, type(None), dict]:
         if type(obj) in [str, float, int, bool, type(None)]:
             return obj
+        if type(obj) in [list]:
+            return [self.serialize(v) for v in obj]
 
         # noinspection PyTypeChecker
         fields: List[Tuple[str, inspect.Parameter]] = list(inspect.signature(type(obj).__init__).parameters.items())[1:]
@@ -16,12 +18,15 @@ class JsonCerealizer(Cerealizer[T, typing.Union[str, float, int, bool, type(None
         return {field: self.serialize(getattr(obj, field)) for field, param in fields}
 
     def deserialize(self, obj: typing.Union[str, float, int, bool, type(None), dict], t: T) -> T:
-        if type(obj) in [str, float, int, bool, type(None)]:
+        if t in [str, float, int, bool, type(None)]:
             return obj
+        if typing.get_origin(t) in [list]:
+            return [self.deserialize(v, typing.get_args(t)[0]) for v in obj]
 
         if typing.get_origin(t) == typing.Union:
-            t1, t2 = typing.get_args(t)
-            return self.deserialize(obj, t1)
+            args = typing.get_args(t)
+            if len(args) == 2 and args[1] is type(None):
+                return self.deserialize(obj, args[0])
 
         # noinspection PyTypeChecker
         fields: List[Tuple[str, inspect.Parameter]] = list(inspect.signature(t.__init__).parameters.items())[1:]
--- tests/super_cereal/cerealizer/test_json.py
+++ tests/super_cereal/cerealizer/test_json.py
@@ -1,5 +1,5 @@
 import dataclasses
-from typing import Optional
+from typing import Optional, List
 
 import pytest
 
@@ -21,11 +21,29 @@ def test_serializer_simple_object():
         field2: int
         field3: float
         field4: bool
-        field5: Optional[str]
-        field6: Optional[str]
+        field6: List[str]
+        field7: Optional[str]
+        field8: Optional[str]
 
     cerealizer = JsonCerealizer()
-    obj = TestClass('stuff', 42, 12.552, True, 'another', None)
+    obj = TestClass('stuff', 42, 12.552, True, ['1', '2', '3'], 'another', None)
+    serialized = cerealizer.serialize(obj)
+    deserialized = cerealizer.deserialize(serialized, TestClass)
+    assert obj == deserialized
+
+
+def test_serializer_complex_object():
+    @dataclasses.dataclass
+    class AnotherClass:
+        field: int
+
+    @dataclasses.dataclass
+    class TestClass:
+        field1: str
+        field2: Optional[List[AnotherClass]]
+
+    cerealizer = JsonCerealizer()
+    obj = TestClass('stuff', [AnotherClass(42), AnotherClass(27)])
     serialized = cerealizer.serialize(obj)
     deserialized = cerealizer.deserialize(serialized, TestClass)
     assert obj == deserialized
