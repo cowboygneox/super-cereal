e5adb76016cef96845ee20dc9c146ec948a4d3a0
Sean Freitag <sean.freitag@avant.com>
2023-01-19 14:05:07 -0600

    Adding in coverage.

--- super_cereal/cerealizer/__init__.py
+++ super_cereal/cerealizer/__init__.py
@@ -5,11 +5,11 @@ T = TypeVar('T')
 V = TypeVar('V')
 
 
-class Cerealizer(abc.ABC, Generic[T, V]):
+class Cerealizer(abc.ABC, Generic[T, V]):  # pragma: no cover
     @abc.abstractmethod
     def serialize(self, obj: T) -> V:
-        ...
+        pass
 
     @abc.abstractmethod
     def deserialize(self, obj: V, t: T) -> T:
-        ...
+        pass
